a3819e6e809fd11b5784620bbe5f2857364e4909
MichaelLuuTrong <michaelluutrong@gmail.com>
2025-03-02 15:36:03 -0800

    added groups and containers to tidy up in preparation for adding more

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     game_clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
 
     player_ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
@@ -19,8 +27,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player_ship.update(dt)
-        player_ship.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = (game_clock.tick(60)) / 1000
 
