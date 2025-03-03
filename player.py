127abf0772c8e8c89275ade89f6753e55542dcdc
MichaelLuuTrong <michaelluutrong@gmail.com>
2025-03-02 16:50:52 -0800

    added bullets to shoot

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS, PLAYER_SHOOT_COOLDOWN
 import pygame
+from shot import Shot
 
 class Player(CircleShape):
-    rotation = 0
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
+        self.timer = 0
+        self.rotation = 0
 
     
     def triangle(self):
@@ -27,6 +29,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
         
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -37,3 +40,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        if self.timer > 0:
+            return
+        else:
+            self.timer = PLAYER_SHOOT_COOLDOWN
+            shot = Shot(self.position[0], self.position[1], SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+    
